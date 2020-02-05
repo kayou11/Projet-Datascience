a5ef4cba7831886805178bc8b0f4d8f9469bb94b
Pierre Kerebel Omer <kayou11@users.noreply.github.com>
2020-02-05 20:52:53 +0100

    Update pipeline.py

--- Pipeline/pipeline.py
+++ Pipeline/pipeline.py
@@ -34,6 +34,6 @@ class Pipeline():
     def download_git_data(self):
         url = "https://github.com/Pielgrin/dataset_clean_degraded.git"
         os.system('git clone %s' %url)
-        os.system('cp dataset_clean_degraded/degraded Train/degraded')
         os.system('mv dataset_clean_degraded Val')
         os.system('mv Val/test_degraded Test')
+        os.system('cp Val/degraded Train/degraded')
